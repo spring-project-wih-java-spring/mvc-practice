cf39e4d86075f2c4167a835035a30fd942f946dd
sjh9391985 <sjh9391985@gmail.com>
2023-11-28 21:01:08 +0900

    feat: "사칙연산"
    - 0으로 나누는 예외 케이스

--- src/main/java/test_code3/calculate/DivOperator.java
+++ src/main/java/test_code3/calculate/DivOperator.java
@@ -11,6 +11,10 @@ public class DivOperator implements NewArithmeticOperator {
 
     @Override
     public int calculate(int operand1, int operand2) {
+        if(operand2 == 0) {
+            throw new IllegalArgumentException("0으로는 나눌수 없습니다.");
+        }
+
         return operand1 / operand2;
     }
 }
