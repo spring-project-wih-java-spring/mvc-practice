2166362daa6b3004fc56dca165ee3b6ab92ae2d9
sjh9391985 <sjh9391985@gmail.com>
2023-11-28 20:56:12 +0900

    feat: "사칙연산"
    - 인터페이스 리팩토링 테스트

--- src/main/java/test_code3/Calculator.java
+++ src/main/java/test_code3/Calculator.java
@@ -1,12 +1,28 @@
 package test_code3;
 
+import test_code3.calculate.AdditionOperator;
+import test_code3.calculate.DivOperator;
+import test_code3.calculate.MulOperator;
+import test_code3.calculate.SubtractionOperator;
+
+import java.util.List;
+
 public class Calculator {
 
+    private static final List<NewArithmeticOperator> arithmeticoperator = null;// = List.of(new AdditionOperator(), new SubtractionOperator(), new MulOperator(), new DivOperator());
     public static int calculate(int opperand1, String operator, int opperand2){
-        int answer = 0;
 
-        return ArithmeticOperator.calculate(opperand1, operator, opperand2);
+        // 방법3
+        return arithmeticoperator.stream()
+                .filter(arithmeticoperator -> arithmeticoperator.supports(operator))
+                .map(arithmeticoperator -> arithmeticoperator.calculate(opperand1, opperand2))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("올바른 사직연산이 아닙니다."));
 
+        // 방법 2
+//        return ArithmeticOperator.calculate(opperand1, operator, opperand2);
+
+        // 방법 1
 //        switch (operator) {
 //            case "+":
 //                answer = opperand1 + opperand2;
---
+++ src/main/java/test_code3/calculate/AdditionOperator.java
@@ -0,0 +1,16 @@
+package test_code3.calculate;
+
+import test_code3.NewArithmeticOperator;
+
+public class AdditionOperator implements NewArithmeticOperator {
+
+    @Override
+    public boolean supports(String operator) {
+        return "+".equals(operator);
+    }
+
+    @Override
+    public int calculate(int operand1, String operator, int operand2) {
+        return operand1 + operand2;
+    }
+}
