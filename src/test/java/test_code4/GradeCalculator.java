b19890c30661009b49b9065d5387029686637527
sjh9391985 <sjh9391985@gmail.com>
2023-11-29 21:07:02 +0900

    feat: "학점 계산 테스트 코드"

---
+++ src/test/java/test_code4/GradeCalculator.java
@@ -0,0 +1,18 @@
+package test_code4;
+
+import java.util.List;
+
+public class GradeCalculator {
+
+    private final List<Course> courses;
+
+    public GradeCalculator(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public double calculate() {
+
+
+
+    }
+}
---
+++ src/test/java/test_code4/GradeCalculatorTest.java
@@ -0,0 +1,31 @@
+package test_code4;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GradeCalculatorTest {
+
+    @Test
+    void calculateGradeTest() {
+        List<Course> courses =
+                Arrays.asList(
+                        new Course("OOP", 3, "A+"),
+                        new Course("자료구조", 3, "A+")
+                );
+
+                /* JAVA9 이상부터 사용가능
+                List.of(
+                new Course("OOP", 3, "A+"),
+                new Course("자료구조", 3, "A+")
+                );
+                */
+
+        GradeCalculator gradeCalculator = new GradeCalculator(courses);
+        double gradeResult = gradeCalculator.calculate();
+
+
+    }
+
+}
