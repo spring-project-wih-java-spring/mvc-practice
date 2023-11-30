ce3e4e426f776c242de9b5f529e5c0003d33db9a
sjh9391985 <sjh9391985@gmail.com>
2023-11-30 19:44:48 +0900

    modify: "메뉴 주문 테스트 코드 리팩토링"

--- src/test/java/test_code5/Cook.java
+++ src/test/java/test_code5/Cook.java
@@ -8,11 +8,16 @@ import java.util.Objects;
 
 public class Cook {
 
-    private final String name;
-    private final int price;
+    private String name;
+    private int price;
     public Cook(String name, int price) {
         this.name = name;
         this.price = price;
+    }
+
+    public Cook(MenuItem menuItem) {
+        this.name = menuItem.getName();
+        this.price = menuItem.getPrice();
     }
 
     @Override
--- src/test/java/test_code5/Cooking.java
+++ src/test/java/test_code5/Cooking.java
@@ -2,7 +2,7 @@ package test_code5;
 
 public class Cooking {
     public Cook makeCook(MenuItem menuItem) {
-
-        return new Cook("돈까스", 5000);
+        Cook cook = new Cook(menuItem);
+        return cook;
     }
 }
