0bc825375971c356c2fa8115e7e0af7d78078e91
sjh9391985 <sjh9391985@gmail.com>
2023-12-04 09:59:11 +0900

    feat: "jdbc insert test"

--- src/main/java/jdbctest/UserDao.java
+++ src/main/java/jdbctest/UserDao.java
@@ -1,7 +1,47 @@
 package jdbctest;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UserDao {
-    public void create(User user) {
+
+    private Connection getConnection() {
+        String url = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+        String id = "sa";
+        String pw = "";
+
+        try {
+            Class.forName("org.h2.Driver");
+            return DriverManager.getConnection(url, id, pw);
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+    public void create(User user) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = getConnection();
+            String sql = "insert into USERS values ( ?, ?, ?, ? )";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, user.getUserId());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3, user.getName());
+            preparedStatement.setString(4, user.getEmail());
+            preparedStatement.executeUpdate();
+        } finally {
+            if(preparedStatement != null) {
+                preparedStatement.close();
+            }
+
+            if(connection != null) {
+                connection.close();
+            }
+        }
     }
 
     public User findByUserId(String wizard) {
--- src/test/java/jdbctest/UserDaoTest.java
+++ src/test/java/jdbctest/UserDaoTest.java
@@ -7,6 +7,8 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
+import java.sql.SQLException;
+
 public class UserDaoTest {
 
     @BeforeEach
@@ -17,7 +19,7 @@ public class UserDaoTest {
     }
 
     @Test
-    void createTest() {
+    void createTest() throws SQLException {
         UserDao userDao = new UserDao();
         userDao.create(new User("wizard", "pw", "name", "email"));
         User user = userDao.findByUserId("wizard");
