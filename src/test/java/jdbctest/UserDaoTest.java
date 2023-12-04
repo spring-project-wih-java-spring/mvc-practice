711c4e58fb5ef98ce3dba7c212cbe84e6d05833f
sjh9391985 <sjh9391985@gmail.com>
2023-12-04 09:42:12 +0900

    feat: "jdbc 테스트 환경구성"

---
+++ src/test/java/jdbctest/UserDaoTest.java
@@ -0,0 +1,17 @@
+package jdbctest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+public class UserDaoTest {
+
+    @BeforeEach
+    void setUp() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource("db_schema.sql"));
+        DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+    }
+}
