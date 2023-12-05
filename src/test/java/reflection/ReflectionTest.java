c2ce566855f4d2761d81ada0f462e48ddb69eb7f
sjh9391985 <sjh9391985@gmail.com>
2023-12-05 23:04:16 +0900

    feat: "객체 모델 정보 확인 테스트"

--- src/test/java/reflection/ReflectionTest.java
+++ src/test/java/reflection/ReflectionTest.java
@@ -6,12 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reflection.anotation.Controller;
 import reflection.anotation.Service;
+import reflection.model.User;
 
 import java.lang.annotation.Annotation;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ReflectionTest {
 
@@ -21,6 +23,13 @@ public class ReflectionTest {
     void controllerScan() {
         Set<Class<?>> beans = getTypesAnnotatedWith(Arrays.asList(Controller.class, Service.class));
         logger.debug("beans : [{}]", beans);
+    }
+
+    @Test
+    void showClass() {
+        Class<User> userClass = User.class;
+        logger.debug(userClass.getName());
+        logger.debug("User all declared fields: [{}]", Arrays.stream(userClass.getDeclaredConstructors()).collect(Collectors.toList()));
     }
 
     private static Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
