25a3a77acba5ed55cb80c16b9d7ee03f441c6741
sjh9391985 <sjh9391985@gmail.com>
2023-12-05 22:18:18 +0900

    feat: "@Controller 붙은 클래스 찾기"

---
+++ src/test/java/reflection/ReflectionTest.java
@@ -0,0 +1,25 @@
+package reflection;
+
+import org.junit.jupiter.api.Test;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reflection.anotation.Controller;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ReflectionTest {
+
+    private static  final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
+
+    @Test
+    void controllerScan() {
+        Reflections reflections = new Reflections("org.example");
+
+        Set<Class<?>> beans = new HashSet<>();
+        beans.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+
+        logger.debug("beans : [{}]", beans);
+    }
+}
