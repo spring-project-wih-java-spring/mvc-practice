f340fd3fd29b6ba0a32862f9973492e4267c7fb2
sjh9391985 <sjh9391985@gmail.com>
2023-12-07 21:11:08 +0900

    feat: "Redirect View 개발"

---
+++ src/main/java/front_pattern/mvc/view/RedirectView.java
@@ -0,0 +1,22 @@
+package front_pattern.mvc.view;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+public class RedirectView implements View{
+
+    public static final String REDIRECT_PREFIX = "redirect:";
+    private final String name;
+
+    public RedirectView(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                response.sendRedirect(name.substring(REDIRECT_PREFIX.length()));
+    }
+}
