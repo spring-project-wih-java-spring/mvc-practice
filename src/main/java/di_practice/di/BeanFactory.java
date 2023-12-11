3f8a13123bad1d5c6c4fd49dcfebc7c001309215
sjh9391985 <sjh9391985@gmail.com>
2023-12-11 12:16:26 +0900

    feat: "인스턴스 생성 메서드 개발"

--- src/main/java/di_practice/di/BeanFactory.java
+++ src/main/java/di_practice/di/BeanFactory.java
@@ -1,15 +1,48 @@
 package di_practice.di;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import di_practice.annotation.Inject;
+import org.reflections.ReflectionUtils;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 
 public class BeanFactory {
     private final Set<Class<?>> preInstantiatedClass;
-    private Map<Class<?>, Object> beans=  new HashMap<>();
+    private Map<Class<?>, Object> beans = new HashMap<>();
 
     public BeanFactory(Set<Class<?>> preInstantiatedClass) {
         this.preInstantiatedClass = preInstantiatedClass;
+        initialize();
+    }
+
+    private void initialize() {
+        for (Class<?> clazz : preInstantiatedClass) {
+            Object instance = createInstance(clazz);
+            beans.put(clazz, instance);
+        }
+    }
+
+    private Object createInstance(Class<?> clazz) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+        // 생성자
+        Constructor<?> constructor = findConstructor(clazz);
+
+        // 파라미터
+        List<Object> parameters = new ArrayList<>();
+        for (Class<?> typeClass : constructor.getParameterTypes()){
+            parameters.add(getParameterByClass(typeClass));
+        }
+
+        // 인스턴스 생성
+        return constructor.newInstance(parameters.toArray());
+    }
+
+    private Constructor<?> findConstructor(Class<?> clazz) {
+        Set<Constructor> injectedConstructors = ReflectionUtils.getAllConstructors(clazz, ReflectionUtils.withAnnotation(Inject.class));
+        if(injectedConstructors.isEmpty()) {
+            return null;
+        }
+        return injectedConstructors.iterator().next();
     }
 
     public <T> T getBean(Class<T> requiredType) {
