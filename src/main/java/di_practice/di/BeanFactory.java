a5242e2c0da9c8422b7265e768ef0c79091c19da
sjh9391985 <sjh9391985@gmail.com>
2023-12-11 12:03:52 +0900

    feat: "빈 팩토리 테스트 코드"

--- src/main/java/di_practice/di/BeanFactory.java
+++ src/main/java/di_practice/di/BeanFactory.java
@@ -1,4 +1,10 @@
 package di_practice.di;
 
+import java.util.Set;
+
 public class BeanFactory {
+    private final Set<Class<?>> preInstantiatedClass;
+    public BeanFactory(Set<Class<?>> preInstantiatedClass) {
+        this.preInstantiatedClass = preInstantiatedClass;
+    }
 }
--- src/test/java/di_practice/di/BeanFactoryTest.java
+++ src/test/java/di_practice/di/BeanFactoryTest.java
@@ -1,7 +1,32 @@
 package di_practice.di;
 
+import di_practice.annotation.Controller;
+import di_practice.annotation.Service;
+import org.junit.jupiter.api.BeforeEach;
+import org.reflections.Reflections;
+
+import java.lang.annotation.Annotation;
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BeanFactoryTest {
+    private Reflections reflections;
+    private BeanFactory beanFactory;
 
+    @BeforeEach
+    void setUp() {
+        reflections = new Reflections("di_practice");
+        Set<Class<?>> preInstantiatedClass = getTypesAnnotatedWith(Controller.class, Service.class);
+        beanFactory = new BeanFactory(preInstantiatedClass);
+    }
+
+    private Set<Class<?>> getTypesAnnotatedWith(Class<? extends Annotation>... annotations) {
+        Set<Class<?>> beans = new HashSet<>();
+        for(Class<? extends Annotation> annotation : annotations) {
+            beans.addAll(reflections.getTypesAnnotatedWith(annotation));
+        }
+        return beans;
+    }
 }
